7432759b4a30abc14d037c0fa296ea4e5c77b52b
Hüseyin Güney <guneyin@gmail.com>
2024-11-30 01:00:28 +0300

    db reverted to mysql

--- db/db.go
+++ db/db.go
@@ -3,7 +3,7 @@ package database
 import (
 	"fmt"
 	"github.com/guneyin/locator/config"
-	"gorm.io/driver/postgres"
+	"gorm.io/driver/mysql"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
@@ -11,15 +11,11 @@ import (
 
 var gormConfig = &gorm.Config{Logger: logger.Default.LogMode(logger.Error)}
 
-//func NewDB(cfg *config.Config) (*gorm.DB, error) {
-//	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/gorm?charset=utf8&parseTime=True&loc=Local", cfg.DbUser, cfg.DbPassword, cfg.DbHost, cfg.Port)
-//	return gorm.Open(mysql.Open(dsn), gormConfig)
-//}
-
 func NewDB(cfg *config.Config) (*gorm.DB, error) {
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=disable TimeZone=Asia/Shanghai",
-		cfg.DbHost, cfg.DbUser, cfg.DbPassword, cfg.DbName, cfg.DbPort)
-	return gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local",
+		cfg.DbUser, cfg.DbPassword, cfg.DbHost, cfg.DbPort, cfg.DbName)
+
+	return gorm.Open(mysql.Open(dsn), gormConfig)
 }
 
 func NewTestDB() (*gorm.DB, error) {
